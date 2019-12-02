bce0f796b75c291d6f6cb317927fd0dc266fe2d8
Gustav <gustav@Jaakkos-MBP.localdomain>
2019-12-02 11:53:50 +0100

    Add training script for 1 percent of the training data

---
+++ scripts/run_1_supervised.sh
@@ -0,0 +1,11 @@
+python main.py \
+  --do_eval_along_training=True \
+  --sup_cut=0.01 \
+  --unsup_cut=0. \
+  --unsup_ratio=0 \
+  --shuffle_seed=42 \
+  --train_batch_size=4 \
+  --train_steps=100000 \
+  --max_save=10 \
+  --data_dir=data/processed_data \
+  --model_dir=ckpt/5_supervised
