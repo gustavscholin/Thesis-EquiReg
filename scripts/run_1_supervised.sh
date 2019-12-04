db3a068fe047f7e6a2c063589bdd75c5f300d120
Gustav <gustav@Jaakkos-MBP.localdomain>
2019-12-04 10:31:35 +0100

    Do training and predict in one script

--- scripts/run_1_supervised.sh
+++ scripts/run_1_supervised.sh
@@ -1,5 +1,9 @@
+model_dir=ckpt/1_supervised
+data_dir=data/processed_data
+
 python main.py \
   --do_eval_along_training=True \
+  --do_predict=False \
   --sup_cut=0.01 \
   --unsup_cut=0. \
   --unsup_ratio=0 \
@@ -7,5 +11,13 @@ python main.py \
   --train_batch_size=4 \
   --train_steps=100000 \
   --max_save=10 \
-  --data_dir=data/processed_data \
-  --model_dir=ckpt/1_supervised
+  --data_dir=${data_dir} \
+  --model_dir=${model_dir}
+
+python main.py \
+  --do_eval_along_training=False \
+  --do_predict=True \
+  --data_dir=${data_dir} \
+  --eval_batch_size=16 \
+  --model_dir=${model_dir} \
+  --pred_dataset=val
