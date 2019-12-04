a0ddf6dbb5ab8104e0764f6de869b51482adde8f
Gustav <gustav@Jaakkos-MBP.localdomain>
2019-12-04 17:06:56 +0100

    Script for unsupervised training

---
+++ scripts/run_uda.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+sup_cut=$1
+unsup_cut=$2
+model_dir="ckpt/${sup_cut}_${unsup_cut}_uda"
+data_dir=data/processed_data
+
+python main.py \
+  --do_eval_along_training=True \
+  --do_predict=False \
+  --sup_cut=${sup_cut} \
+  --unsup_cut=${unsup_cut} \
+  --unsup_ratio=3 \
+  --shuffle_seed=42 \
+  --train_batch_size=4 \
+  --train_steps=100000 \
+  --max_save=1 \
+  --data_dir=${data_dir} \
+  --model_dir=${model_dir}
+
+python main.py \
+  --do_eval_along_training=False \
+  --do_predict=True \
+  --data_dir=${data_dir} \
+  --eval_batch_size=16 \
+  --model_dir=${model_dir} \
+  --pred_dataset=val
