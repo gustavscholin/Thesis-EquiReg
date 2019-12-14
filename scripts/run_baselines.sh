86c1db8d76b6feba44e99f5c0dda22e034a33330
Gustav <gustav@Jaakkos-MBP.localdomain>
2019-12-14 17:36:01 +0100

    no lr dec

--- scripts/run_baselines.sh
+++ scripts/run_baselines.sh
@@ -2,11 +2,11 @@
 sup_cuts=(0.01 0.05 0.1 1.0)
 data_dir=data/processed_data
 save_steps=(125 250 250 500)
-dec_steps=(2000 5000 7000 50000)
+# dec_steps=(2000 5000 7000 50000)
 
 for experiment_number in {1..3}; do
-  for sup_cut in {0..4}; do
-    model_dir="ckpt/new_baseline_${sup_cut}_supervised_${experiment_number}"
+  for sup_cut in {0..3}; do
+    model_dir="ckpt/new_baseline_${sup_cuts[sup_cut]}_supervised_${experiment_number}"
 
     python main.py \
       --do_eval_along_training=True \
@@ -21,7 +21,7 @@ for experiment_number in {1..3}; do
       --max_save=1 \
       --data_dir=${data_dir} \
       --model_dir=${model_dir} \
-      --cos_lr_dec_steps=${dec_steps[sup_cut]}
+      --cos_lr_dec_steps=-1
 
     python main.py \
       --do_eval_along_training=False \
