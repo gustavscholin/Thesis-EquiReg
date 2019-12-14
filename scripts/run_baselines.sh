9231d733ee7907ecd43a18a414931bbf389ec363
Gustav <gustav@Gustavs-MacBook-Pro-2.local>
2019-12-14 20:25:30 +0100

    some updates to the run bash scripts

--- scripts/run_baselines.sh
+++ scripts/run_baselines.sh
@@ -5,19 +5,19 @@ save_steps=(125 250 250 500)
 # dec_steps=(2000 5000 7000 50000)
 
 for experiment_number in {1..3}; do
-  for sup_cut in {0..3}; do
-    model_dir="ckpt/new_baseline_${sup_cuts[sup_cut]}_supervised_${experiment_number}"
+  for i in {0..3}; do
+    model_dir="ckpt/new_baseline_${sup_cuts[i]}_supervised_${experiment_number}"
 
     python main.py \
       --do_eval_along_training=True \
       --do_predict=False \
-      --sup_cut=${sup_cuts[sup_cut]} \
+      --sup_cut=${sup_cuts[i]} \
       --unsup_cut=0. \
       --unsup_ratio=0 \
       --shuffle_seed=42 \
       --train_batch_size=4 \
       --train_steps=50000 \
-      --save_steps=${save_steps[sup_cut]} \
+      --save_steps=${save_steps[i]} \
       --max_save=1 \
       --data_dir=${data_dir} \
       --model_dir=${model_dir} \
--- scripts/run_uda.sh
+++ scripts/run_uda.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
-sup_cut=$1
-unsup_cut=$2
-model_dir="ckpt/${sup_cut}_${unsup_cut}_uda"
+sup_cut=(0.05 0.1)
+unsup_cut=(0.95 0.9)
 data_dir=data/processed_data
 
-python main.py \
-  --do_eval_along_training=True \
-  --do_predict=False \
-  --sup_cut=${sup_cut} \
-  --unsup_cut=${unsup_cut} \
-  --unsup_ratio=2 \
-  --shuffle_seed=42 \
-  --train_batch_size=1 \
-  --train_steps=50000 \
-  --max_save=1 \
-  --data_dir=${data_dir} \
-  --model_dir=${model_dir} \
-  --unsup_coeff=1 \
-  --tsa= \
-  --early_stop_steps=10000 \
-  --unsup_crop=True \
-  --cos_lr_dec_steps=-1
+for i in {0..1}; do
+  model_dir="ckpt/${sup_cut[i]}_${unsup_cut[i]}_uda"
 
-python main.py \
-  --do_eval_along_training=False \
-  --do_predict=True \
-  --data_dir=${data_dir} \
-  --eval_batch_size=16 \
-  --model_dir=${model_dir} \
-  --pred_dataset=val
+  python main.py \
+    --do_eval_along_training=True \
+    --do_predict=False \
+    --sup_cut=${sup_cut[i]} \
+    --unsup_cut=${unsup_cut[i]} \
+    --unsup_ratio=2 \
+    --shuffle_seed=42 \
+    --train_batch_size=1 \
+    --train_steps=50000 \
+    --max_save=1 \
+    --data_dir=${data_dir} \
+    --model_dir=${model_dir} \
+    --unsup_coeff=1 \
+    --tsa= \
+    --early_stop_steps=10000 \
+    --unsup_crop=True \
+    --cos_lr_dec_steps=-1
+
+  python main.py \
+    --do_eval_along_training=False \
+    --do_predict=True \
+    --data_dir=${data_dir} \
+    --eval_batch_size=16 \
+    --model_dir=${model_dir} \
+    --pred_dataset=val
+done
