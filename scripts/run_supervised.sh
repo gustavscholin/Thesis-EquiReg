cec71f8fb532e5d09bb028ff728d77f5780a1f00
Gustav <gustav@Jaakkos-MBP.localdomain>
2020-01-10 10:21:46 +0100

    re-running failed experiment

--- scripts/run_supervised.sh
+++ scripts/run_supervised.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-sup_cut=$1
-model_dir="ckpt/${sup_cut}_supervised"
+sup_cut=0.01
+experiment_number=1
+seed=43
+save_steps=125
+early_stop_steps=2000
+model_dir="ckpt/baseline_${sup_cut}_${experiment_number}_seed_${seed}"
 data_dir=data/processed_data
 
 python main.py \
@@ -9,12 +13,16 @@ python main.py \
   --sup_cut=${sup_cut} \
   --unsup_cut=0. \
   --unsup_ratio=0 \
-  --shuffle_seed=42 \
+  --shuffle_seed=${seed} \
   --train_batch_size=4 \
   --train_steps=100000 \
+  --save_steps=${save_steps} \
   --max_save=1 \
   --data_dir=${data_dir} \
-  --model_dir=${model_dir}
+  --model_dir=${model_dir} \
+  --early_stop_steps=${early_stop_steps}  \
+  --min_step=0 \
+  --exp_lr_decay=False
 
 python main.py \
   --do_eval_along_training=False \
