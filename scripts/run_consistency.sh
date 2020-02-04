651f0c3a4b131ba4fc2cda67e3d4dd372d477916
gustavscholin <scholin.gustav@gmail.com>
2020-02-04 09:56:37 +0100

    Use 100% of the data unsup

--- scripts/run_consistency.sh
+++ scripts/run_consistency.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sup_cut=(0.01 0.05 0.1)
-unsup_cut=(0.99 0.95 0.9)
+# unsup_cut=(0.99 0.95 0.9)
 # min_steps=(0 50000 50000)
 data_dir=data/processed_data
 early_stop_steps=(10000 20000 30000)
@@ -8,13 +8,13 @@ early_stop_steps=(10000 20000 30000)
 for experiment_nbr in {1..3}; do
   for seed in {42..44}; do
     for i in {0..2}; do
-      model_dir="/mnt/storage/data/thesis-uda/ckpt/consistency/consistency_${sup_cut[i]}_${unsup_cut[i]}_${experiment_nbr}_seed_${seed}"
+      model_dir="/mnt/storage/data/thesis-uda/ckpt/consistency/consistency_${sup_cut[i]}_${experiment_nbr}_seed_${seed}"
 
       python main.py \
         --do_eval_along_training=True \
         --do_predict=False \
         --sup_cut=${sup_cut[i]} \
-        --unsup_cut=${unsup_cut[i]} \
+        --unsup_cut=1.0 \
         --unsup_ratio=3 \
         --shuffle_seed=${seed} \
         --train_batch_size=2 \
